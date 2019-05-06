5ad65803b6b9cef132060dad525419b26252a896
Bob Baker <31376573+rbaker26@users.noreply.github.com>
2019-05-05 20:08:00 -0700

    working rest with sql

---
+++ ProductsREST/ProductsREST/Controllers/ValuesController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProductsREST.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> Get()
+        {
+
+            return SQL_Interface.Instance.GetProducts();
+
+            //catch(Exception e)
+            //{
+            //    Console.Out.WriteLine("\n\n**********************************************************************");
+            //    Console.Out.WriteLine(e.Message);
+            //    Console.Out.WriteLine(e.InnerException);
+            //    Console.Out.WriteLine(e.Source);
+            //    Console.Out.WriteLine("**********************************************************************\n\n");
+            //    return SQL_Interface.Instance.GetProducts();
+            //}
+
+        
+
+            // return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ProductsREST/ProductsREST/SQL_Interface.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+using System.Data.SQLite;
+
+
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace ProductsREST
+{
+    public sealed class SQL_Interface
+    {
+        //*****************************************************************************************
+        private static MySqlConnection m_dbConnection = null;
+
+        private static readonly Lazy<SQL_Interface>
+        m_lazy = new Lazy<SQL_Interface>(() => new SQL_Interface());
+
+        public static SQL_Interface Instance { get { return m_lazy.Value; } }
+        //*****************************************************************************************
+
+
+        //*****************************************************************************************
+        private SQL_Interface()
+        {
+            // SQLiteConnection.CreateFile("MyDatabase.sqlite");
+            // this.m_dbConnection = new SQLiteConnection("Data Source=C:\\Users\\007ds\\Documents\\GitHub\\RestAPI-Store\\ProductsREST\\ProductsREST\\products.db;Version=3;");
+            m_dbConnection = new MySqlConnection("Server=68.5.123.182; database=productsREST_db; UID=recorder; password=recorder0");
+            m_dbConnection.Open();
+        }
+        //*****************************************************************************************
+
+
+        //*****************************************************************************************
+        ~SQL_Interface()
+        {
+            try
+            {
+              m_dbConnection.Close();
+            }
+            catch (Exception e)
+            {
+                Console.Out.WriteLine(e.Message);
+            }
+
+        }
+        //*****************************************************************************************
+
+        public List<Product> GetProducts()
+        {
+            List<Product> products = new List<Product>();
+            //products.Add(new Product(1, "Hi", 55, 128.20f));
+
+            // SQLiteDataReader sqlite_datareader;
+            MySqlDataReader mysql_datareader;
+            try
+            {
+                string query = "SELECT product_id, description, quantity, price FROM products";
+                //string query = "SELECT * FROM products";
+               // SQLiteCommand command = m_dbConnection.CreateCommand();
+                MySqlCommand command = m_dbConnection.CreateCommand();
+                command.CommandText = query;
+              
+                mysql_datareader = command.ExecuteReader();
+
+                Product temp = new Product();
+
+               
+                while (mysql_datareader.Read())
+                {
+                    temp.ProductId = mysql_datareader.GetInt32(0);
+                    temp.Description = mysql_datareader.GetString(1);
+                    temp.Quantity = mysql_datareader.GetInt32(2);
+                    temp.Price = mysql_datareader.GetFloat(3);
+
+
+                    //*************************
+                    //* Debug Code            *
+                    //*************************
+                    Console.WriteLine("\n\n\n\n****************************************************************************");
+                    Console.WriteLine(temp);
+                    //*************************
+
+                    products.Add(temp);
+                    temp = new Product();
+                }
+                mysql_datareader.Close();
+            }
+            catch (Exception e)
+            {
+                Console.Out.WriteLine("\n\n**********************************************************************");
+                Console.Out.WriteLine(e.Message);
+                Console.Out.WriteLine(e.InnerException);
+                Console.Out.WriteLine(e.Source);
+                Console.Out.WriteLine("**********************************************************************\n\n");
+
+            }
+            
+
+            return products;
+        }
+
+    }
+}
