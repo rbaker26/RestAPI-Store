1498a4e0a892a981872ccc9aaaa14d6874d9438f
Bob Baker <31376573+rbaker26@users.noreply.github.com>
2019-05-07 10:59:44 -0700

    added anotations and updated order and cart

--- REST-API-solution/REST_lib/Cart.cs
+++ REST-API-solution/REST_lib/Cart.cs
@@ -2,12 +2,22 @@
 using System.Collections.Generic;
 using System.Text;
 
+using System.Runtime.Serialization;
+
 namespace REST_lib
 {
+    [DataContract]
     public class Cart
     {
+        [DataMember]
         public string Email { get; set; }
-        public List<KeyValuePair<Product,Int32>> ProductsCart { get; set; }
 
+        /// <summary>
+        /// <ProductID, QuantityPurchased>
+        /// This way the client cannot change things like the price of quantity on hacd
+        /// </summary>
+        [DataMember]
+        public List<KeyValuePair<Int32,Int32>> ShoppingCart { get; set; }
+        
     }
 }
