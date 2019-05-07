9f3e923545b2a2ac38c675ffdc4d04791a34a2e5
Bob Baker <31376573+rbaker26@users.noreply.github.com>
2019-05-07 10:45:40 -0700

    added json converter

---
+++ REST-API-solution/REST_lib/JsonConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.IO;
+using System.Runtime.Serialization.Json;
+namespace REST_lib
+{
+    class JsonConverter
+    {
+        public static string ToJason<T>(T t)
+        {
+            MemoryStream ms = new MemoryStream();
+            DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(T));
+
+            ser.WriteObject(ms,t);
+
+            ms.Position = 0;
+            StreamReader sr = new StreamReader(ms);
+            return sr.ReadToEnd();
+        }
+
+        public static T FromJson<T>(string s)
+        {
+            T t;
+
+            MemoryStream ms = new MemoryStream();
+            DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(T));
+
+
+            ms.Position = 0;
+            t = (T)ser.ReadObject(ms);
+
+            return t;
+        }
+
+
+    }
+}
