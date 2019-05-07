c445456ab3530501f8fed23320d04e82a80e0e40
Bob Baker <31376573+rbaker26@users.noreply.github.com>
2019-05-06 23:19:51 -0700

    updated urls

--- REST-API-solution/REST-API-solution/Program.cs
+++ REST-API-solution/REST-API-solution/Program.cs
@@ -19,6 +19,7 @@ namespace REST_API_solution
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseUrls("http://localhost:5000", "http://*:5000");
     }
 }
