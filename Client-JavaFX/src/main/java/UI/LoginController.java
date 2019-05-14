9c37227995e284fb3eeb2fdf80468f0cc7051cf2
naomidonato <36245167+naomidonato@users.noreply.github.com>
2019-05-13 22:13:30 -0700

    added the json Converter class and modified ui

--- Client-JavaFX/src/main/java/UI/LoginController.java
+++ Client-JavaFX/src/main/java/UI/LoginController.java
@@ -1,5 +1,6 @@
 package UI;
 
+import com.google.gson.Gson;
 import javafx.beans.InvalidationListener;
 import javafx.beans.Observable;
 import javafx.collections.ObservableList;
@@ -7,11 +8,17 @@ import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import org.controlsfx.control.PopOver;
+import Messages.Converter;
+import UI.Product;
+
+
+import java.util.ArrayList;
 
 public class LoginController extends AbstractSceneController {
 
@@ -26,6 +33,7 @@ public class LoginController extends AbstractSceneController {
     private double mouseX;
     private double mouseY;
 
+    TableView<Product> productInfoTableView;
 
     public LoginController() {
 
@@ -49,10 +57,13 @@ public class LoginController extends AbstractSceneController {
         Label qtyEnteredLabel = new Label("How many would you like to order?: ");
 
         Label descriptionLabel = new Label("Description: ");
+        descriptionLabel.setStyle("-fx-font-weight: bold");
 
         Label priceLabel = new Label("Price: ");
+        priceLabel.setStyle("-fx-font-weight: bold");
 
         Label quantityLabel = new Label("Quantity: ");
+        quantityLabel.setStyle("-fx-font-weight: bold");
 
         emailField = new TextField();
         emailField.setPrefColumnCount(10);
@@ -66,24 +77,61 @@ public class LoginController extends AbstractSceneController {
         popUp.setAutoHide(false);
         popUp.setDetachable(true);
 
-
         enterButton = new Button("Sign In");
-
 
         addToCartButton = new Button("Add To Cart");
 
+        String value = "hello there dof\n" +
+                       "osjdofjddfsdfds\n" +
+                       "oifjdosifjosdij\n" +
+                       "isdjdfdsfdsfdsf\n" +
+                       "foijsdodsfdsfds";
+
+        //Labels for JSon objects to get stored:
+        Label descriptionObj = new Label();
+        Label priceObj = new Label();
+        Label quantityObj = new Label();
+
+        //testing purposes
+        priceObj.setText("" + 2);
+        quantityObj.setText("" + 33);
+
+        TableColumn<Product, String> product_idCol = new TableColumn<>("Product ID");
+        product_idCol.setMinWidth(150);
+        product_idCol.setCellValueFactory(new PropertyValueFactory<>("ProductId"));
+
+        TableColumn<Product, String> descriptionCol = new TableColumn<>("Description");
+        descriptionCol.setMinWidth(150);
+        descriptionCol.setCellValueFactory(new PropertyValueFactory<>("Description"));
+
+        TableColumn<Product, String> quantityCol = new TableColumn<>("Quantity");
+        quantityCol.setMinWidth(150);
+        quantityCol.setCellValueFactory(new PropertyValueFactory<>("Quantity"));
+
+        TableColumn<Product, String> priceCol = new TableColumn<>("Price");
+        priceCol.setMinWidth(150);
+        priceCol.setCellValueFactory(new PropertyValueFactory<>("Price"));
+
+
+        productInfoTableView = new TableView<>();
+        productInfoTableView.getColumns().addAll(product_idCol, descriptionCol, quantityCol, priceCol);
+
         desGrid.setAlignment(Pos.CENTER);
-        desGrid.add(descriptionLabel,5, 0, 1, 2);
-        desGrid.add(priceLabel, 5, 2, 1, 2);
-        desGrid.add(quantityLabel, 5, 4, 1, 1);
-        desGrid.add(qtyEnteredLabel, 5, 6, 1, 1);
-        desGrid.add(quantityField, 7, 6, 1, 1);
-        desGrid.add(addToCartButton, 5, 8, 1, 2);
+      //  desGrid.add(descriptionLabel,5, 0, 1, 2);
+      //  desGrid.add(descriptionObj, 5, 2, 1 , 3);
+       // desGrid.add(priceLabel, 5, 6, 1, 2);
+        //desGrid.add(priceObj, 7, 6, 1, 2);
+        //desGrid.add(quantityLabel, 5, 8, 1, 2);
+        //desGrid.add(quantityObj, 7, 8, 1, 2);
+
+        desGrid.add(productInfoTableView, 5, 1, 1, 1);
+        desGrid.add(qtyEnteredLabel, 5, 5, 1, 1);
+        desGrid.add(quantityField, 5, 6, 1, 1);
+        desGrid.add(addToCartButton, 5, 7, 1, 2);
 
         desGrid.setMinSize(300, 200);
 
         popUp.setContentNode(desGrid);
-
 
         ScrollPane scrollPane = new ScrollPane(productList);
 
