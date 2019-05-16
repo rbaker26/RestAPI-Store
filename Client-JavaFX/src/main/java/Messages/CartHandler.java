a6fdb1c424afbda42310f72da82f4088557d8e75
Bob Baker <31376573+rbaker26@users.noreply.github.com>
2019-05-15 20:55:01 -0700

    adde dblank functions

---
+++ Client-JavaFX/src/main/java/Messages/CartHandler.java
@@ -0,0 +1,24 @@
+package Messages;
+
+import data.CartUpdate;
+import data.RemoveCartUpdate;
+
+public class CartHandler {
+
+    public static void SendCartUpdate(CartUpdate cartUpdate)
+    {
+
+    }
+
+    public static void  PurchaseCart(String email)
+    {
+
+    }
+
+    public static void RemoveCartUpdate(RemoveCartUpdate removeCartUpdate)
+    {
+        
+    }
+
+
+}
